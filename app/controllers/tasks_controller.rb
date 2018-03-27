1ff913e49159a9af383612081b2acac6735a1b0c
benoit mathonat <benoit.mathonat@gmail.com>
2018-03-27 15:12:42 +0200

    Add edit route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,28 @@
+# task controller
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+  def edit
+    # URL: /tasks/1/edit
+    # params
+    # => { id: '1' }
+
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
 end
