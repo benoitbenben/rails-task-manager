d1b26550b58397dc9d465475b45216d3827eece9
benoit mathonat <benoit.mathonat@gmail.com>
2018-03-27 15:59:35 +0200

    add some features

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,24 +5,41 @@ class TasksController < ApplicationController
   end
 
   def show
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
   end
 
   def edit
-    # URL: /tasks/1/edit
-    # params
-    # => { id: '1' }
-
     @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :details, :completed)
+    params.require(:task).permit(:name, :address, :stars)
   end
-
-  # def set_restaurant
-  #   @restaurant = Restaurant.find(params[:id])
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,28 +2,24 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
-  # READ : as a user i can list all tasks
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-
   # # HTTP_VERB URL, to: 'CTRL#ACTION'
   # get 'contact', to: 'pages#contact'
   # get 'about', to: 'pages#about'
 
-  # # CREATE
-  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
-  # post 'restaurants', to: 'restaurants#create'
+  # CREATE
+  get 'tasks/new', to: 'tasks#new', as: :new_restaurant
+  post 'tasks', to: 'tasks#create'
 
-  # # READ
-  # get 'restaurants', to: 'restaurants#index'
-  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # READ : as a user i can list all tasks
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
 
   # EDIT
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
 
-  # # DELETE
-  # delete 'restaurants/:id', to: 'restaurants#destroy'
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 
-  # # resources :restaurants #, only: [:index, :show]
+  # resources :tasks #, only: [:index, :show]
 end
